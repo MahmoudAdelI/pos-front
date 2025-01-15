9e4f9a02657e781cbbb7c84e30959c940c7e1a4d
MahmoudAdelI <mahmoud.adeli7888@gmail.com>
2025-01-15 18:41:35 +0200

    Refactor: Fixed the Navbar animations

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 "use client";
+import classNames from "classnames";
 import Link from "next/link";
 import { useState } from "react";
 import { BsBuildingFill } from "react-icons/bs";
@@ -17,18 +18,35 @@ const navItems = [
   { label: "Products", icon: IoCart, href: "/" },
   { label: "Users", icon: FaUsersCog, href: "/" },
 ];
+
 const Navbar = () => {
   const [expanded, setExpanded] = useState(true);
+
   return (
-    <nav className="bg-navBackground border-navBorder sticky z-10 h-[100vh] flex-col border-r pl-4 pr-6 text-lg leading-none">
-      <div className="mb-12 flex cursor-default items-center gap-2 py-3 text-inherit">
-        <span className="text-Primary">
+    <nav
+      className={classNames({
+        "sticky z-10 h-screen flex-col border-r border-navBorder bg-navBackground pl-4 pr-6 text-lg leading-none transition-all duration-300":
+          true,
+        "w-64": expanded,
+        "w-20": !expanded,
+      })}
+    >
+      <div className="mb-12 flex flex-shrink-0 cursor-default items-center gap-2 overflow-hidden py-3 text-3xl text-inherit">
+        <span className="flex-shrink-0 text-Primary">
           <FaReact />
         </span>
-        {expanded && <div>Logo</div>}
+        <h1
+          className={classNames({
+            "font-bold transition-opacity duration-300": true,
+            "opacity-100": expanded,
+            "opacity-0": !expanded,
+          })}
+        >
+          Logo
+        </h1>
       </div>
       <button
-        className="border-navBorder absolute -right-[17px] rounded-full border bg-inherit p-2 shadow-md"
+        className="absolute -right-[17px] rounded-full border border-navBorder bg-inherit p-2 shadow-md transition-transform duration-300 hover:scale-110"
         onClick={() => setExpanded((prev) => !prev)}
       >
         {expanded ? (
@@ -40,16 +58,14 @@ const Navbar = () => {
       {navItems.map((item, index) => (
         <div
           key={index}
-          className="text-SecondaryTextColor hover:border-Primary hover:bg-highlight hover:text-PrimaryTextColor border-SecondaryTextColor group mb-2 flex w-full cursor-pointer items-center gap-2 rounded-md border-l p-2 transition duration-300"
+          className="group mb-2 flex w-full cursor-pointer items-center gap-2 rounded border-l-2 border-SecondaryTextColor p-2 text-SecondaryTextColor transition-all duration-300 hover:border-Primary hover:bg-highlight hover:text-PrimaryTextColor"
         >
-          <span className="group-hover:text-Icons">
+          <span className="flex-shrink-0 text-xl group-hover:text-Icons">
             <item.icon />
           </span>
-          {expanded && (
-            <div className="mr-16">
-              <Link href={item.href}>{item.label}</Link>
-            </div>
-          )}
+          <div className="overflow-hidden">
+            <Link href={item.href}>{item.label}</Link>
+          </div>
         </div>
       ))}
     </nav>
